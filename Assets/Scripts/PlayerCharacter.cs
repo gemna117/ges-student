06cde5a2b39bdba0b54f293b7c538127c7076309
Megan Alonzo <Megan.Alonzo@iam.local>
2018-10-03 15:13:28 -0500

    2d side scroller

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class PlayCharacter : MonoBehaviour {
 
+    [SerializeField]
+    private int lives = 3; //PacalCase or camelCase
+
+    [SerializeField]
+    private string name = "Fern";
+
+    private float jumpHeight = 5, speed = 5;
+
+    private bool hasKey;
+
+    private bool isOnGround;
+
+    private Rigidbody2D rigidbody2DInstance;
+
 	// Use this for initialization
 	void Start () {
-        Debug.Log("hello");
+
+        rigidbody2DInstance = GetComponent<Rigidbody2D>();
+        rigidbody2DInstance.gravityScale = 5;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update () {
+
+        //transform.Translate(0, -.01f, 0);
+        GetInput();
+        MoveLeft();
+            
 	}
+
+    private void GetInput()
+    {
+        float horizontalInput = Input.GetAxis("horizontal");
+    }
+
+    private void MoveLeft()
+    {
+        rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+    }
 }
