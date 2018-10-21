eec10aa3d8f77e68c3bd22c7f6624706e1c60f1c
megan alonzo <megan.alonzo@loop.colum.edu>
2018-10-21 15:28:34 -0500

    hazard script

---
+++ 2d side scroller/Assets/Scripts/Hazard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("player entered hazard");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+        Debug.Log("something other than the player entered the hazard");
+
+        }
+    }
+}
