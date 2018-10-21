5aa6b41ab3fe4d3e6e25014936ce2a2e45477114
megan alonzo <megan.alonzo@loop.colum.edu>
2018-10-21 14:52:39 -0500

    player physics material

--- 2d side scroller/Assets/Scripts/PlayerCharacter.cs
+++ 2d side scroller/Assets/Scripts/PlayerCharacter.cs
@@ -17,6 +17,12 @@ public class PlayerCharacter : MonoBehaviour {
     private Rigidbody2D rb2d;
 
     [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
     private Collider2D groundDetectTrigger;
 
     [SerializeField]
@@ -33,6 +39,24 @@ public class PlayerCharacter : MonoBehaviour {
         UpdateIsOnGround();
         UpdateHorizontalInput();
         HandleJumpInput();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePhysicsMaterial();
+        Move();
+    }
+
+    private void UpdatePhysicsMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
     }
 
     private void UpdateIsOnGround()
@@ -52,11 +76,6 @@ public class PlayerCharacter : MonoBehaviour {
         {
             rb2d.AddForce(Vector2.up * jumpForce);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
     }
 
     private void Move()
