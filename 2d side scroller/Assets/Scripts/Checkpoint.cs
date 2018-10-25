796c1a997eb78478447b22d066def4fae88d00eb
megan alonzo <megan.alonzo@loop.colum.edu>
2018-10-24 21:18:57 -0500

    checkpoint feedback

--- 2d side scroller/Assets/Scripts/Checkpoint.cs
+++ 2d side scroller/Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour {
 
+    [SerializeField]
+    private float inactivatedrotationSpeed = 100, activatedRotationSpeed = 300;
+
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 1.5f;
+
+    [SerializeField]
+    private Color inactivatedColor, activatedColor;
+
+    private bool isActivated;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedColor;
+        if (isActivated)
+            color = activatedColor;
+
+        spriteRenderer.color = color;
+    }
+
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+
+        if (isActivated)
+            scale = activatedScale;
+
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactivatedrotationSpeed;
+        if (isActivated)
+            rotationSpeed = activatedRotationSpeed;
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+        UpdateColor();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("player"))
