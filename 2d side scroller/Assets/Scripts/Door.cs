4f1b2a0f0cec1735d792a7e2c0fdefcccce585c0
megan alonzo <megan.alonzo@loop.colum.edu>
2018-10-25 21:14:40 -0500

    adding a door

---
+++ 2d side scroller/Assets/Scripts/Door.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    private string SecondSceneLoad;
+    bool isPlayerInTrigger;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            if (Input.GetButtonDown("activate"))
+            {
+                Debug.Log("player activated door");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("activate") && isPlayerInTrigger)
+        {
+            Debug.Log("player activated door");
+            SceneManager.LoadScene("scene to load");
+        }
+    }
+}
