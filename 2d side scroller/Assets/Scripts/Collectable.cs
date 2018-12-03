b74025e72d9159c6a6e08205601c9b3be4a57fa6
megan alonzo <megan.alonzo@loop.colum.edu>
2018-12-03 16:31:33 -0600

    collectables script

---
+++ 2d side scroller/Assets/Scripts/Collectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private int coinCount;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            coinCount++;
+            Debug.Log("coin count: " + coinCount);
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
+        }
+    }
+}
